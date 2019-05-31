df5c3cbdea91e56aeb2fe4fcbdfcffbcddd0f8ee
amusan <amusan83@gmail.com>
2019-05-31 16:53:50 -0400

    added pbj

---
+++ pbj.js
@@ -0,0 +1,55 @@
+// Get out 2 slices of bread, peanut butter,
+// jelly and a knife.
+// Open the jars of peanut butter and jelly.
+// Dip the knife into the peanut butter and
+// spread onto one slice of bread.
+//  Dip the knife into the jelly and spread onto
+// one slice of bread.
+// Put the slices of bread together.
+// Cut the sandwich in half.
+//  Eat and enjoy.
+
+Items = ['bread', 'peanut_butter', 'jelly', 'knife']
+
+function getItems(Items) {
+    console.log('Getting items to make a delicious pb and j sandwhich')
+}
+return Items
+getItems()
+
+function OpenJar(Items) {
+    for (var i = 0; Items.length; i++) {
+        if (item in items === peanut_butter || jelly) {
+            console.log("Opening the jar of peanut_butter and Jelly")
+        }
+    }
+}
+OpenJar()
+
+function Dip(Items)
+for (item in Items) {
+    if (item === knife && item === peanut_butter && item === jelly) {
+        console.log("Dipping knife into peanut_butter and jelly")
+    }
+
+}
+}
+
+function Spread(Items) {
+    for (item in Items) {
+        if (item === knife && item === bread && item === peanut_butter && item === jelly) {
+            console.log('Spreading peanut_butter and Jelly 0n bread')
+        }
+
+    }
+    return Items
+}
+
+function PutBreadTogether(Items) {
+    for (item in Items) {
+        if (item === bread) {
+
+        }
+    }
+
+}
